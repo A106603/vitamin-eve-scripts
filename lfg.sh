706d713eb554a630953acb80b407ed1f34f7d091
A106603 <EVELYN_KIRKENDALL@progressive.com>
2020-10-29 08:39:40 -0600

    add gmail url

--- lfg.sh
+++ lfg.sh
@@ -8,11 +8,10 @@ chrome_open_window() {
 }
 
 # Hello
-echo "Hello Evelyn!"
+printf "\e[36m" && figlet -f cosmic "Hello Evelyn!"
 sleep 2
-printf "\e[36m" && figlet -f doh "LFG!"
+printf "\e[36m" && figlet -f doh "LFG!!"
 sleep 3
-echo
 
 # Weather
 echo "Let's check out the weather real quick. :)"
@@ -25,4 +24,4 @@ echo
 sleep 10
 
 # Open up Chrome tabs
-chrome_open_tab "https://lofimusic.app/chilledcow" "https://github.com/PCDST/AI_ML-Requests/projects/2?card_filter_query=assignee%3AA106603" "http://pgr/sites/highway/Pages/highway.aspx"
+chrome_open_tab "https://lofimusic.app/chilledcow" "https://github.com/PCDST/AI_ML-Requests/projects/2?card_filter_query=assignee%3AA106603" "http://pgr/sites/highway/Pages/highway.aspx" "https://mail.google.com/mail/u/0/?tab=wm&ogbl&safe=active&safe=active#inbox"
