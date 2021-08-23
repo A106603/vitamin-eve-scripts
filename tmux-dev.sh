51911114b08cd08174282301a5c1070031dfd99e
a106603 <A106603@progressive.com>
2021-08-23 15:54:49 -0600

    activate conda env

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+# First argument: name of tmux session
+# Second argument: what conda env to activate
+
 tmux new-session -t $1 -d
 tmux split-window -h
 tmux split-window -v 'ipython'
+
 tmux -2 attach-session -d
+
+if [[ -n $2 ]];
+  conda activate ac3-prize
+fi
